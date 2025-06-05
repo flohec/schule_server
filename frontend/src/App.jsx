0b353b9786bfef42778c5702daeaa7db573e4f44
Florian Heck <flohec027@gmail.com>
2025-06-05 17:02:30 +0200

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gradient-to-br from-blue-400 to-purple-600">
+            <h1 className="text-4xl font-bold text-white">React + Tailwind läufteee! 🚀</h1>
+            <h1>test</h1>
+        </div>
+    );
+}
+
+export default App;
